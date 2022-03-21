f7f58fec297dbc39f595dccdcdba6b017900b36b
rutchen deul <rutchenrodneydeul@gmail.com>
2022-03-21 14:32:45 -0300

    updated text

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -3,7 +3,7 @@ const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the FunSicles NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The FunSicles NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'The FunSicles NFT Project is the first of this kind,Owning a FunSicle will give you mayor benefits in upcoming project,for more information check out our website<a href="https://dev-funsicles.pantheonsite.io/"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
